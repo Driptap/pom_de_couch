ff9259f41974bf908afeddb341768491177496ce
driptap <sg.sinan@gmail.com>
2015-06-07 03:28:12 +0100

    new seeds

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -1,5 +1,7 @@
 class Swipe < ActiveRecord::Base
-
+  belongs_to :user
+  belongs_to :show
   STATES = %w[dislike like watched]
 
+  scope :watched, -> { where(state: "watched") }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
 
 CSV.foreach Rails.root.join("db", "seeds", "shows.csv"), headers: true do |row|
-  Show.create(rovi_id: row[0], title: row[1], description: "This is a description", genre: row[3], img_link: row[4], trailer_link: row[5])
+  Show.create(rovi_id: row[0], title: row[1], description: row[2], genre: row[3], img_link: row[4], trailer_link: row[5], rating: row[6], duration: row[7])
 end
